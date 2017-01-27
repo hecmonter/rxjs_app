fb621c79945f3a237f2d9745f907b063a2237880
Patricia Castillo <montero.hector@gmail.com>
2017-01-28 00:15:15 +0100

    adds arrow functions passed in as arguments to the subscribe method to implement the observer.

--- main.ts
+++ main.ts
@@ -3,20 +3,8 @@ import { Observable } from 'rxjs';
 let numbers = [1, 5, 10, 20]; 
 let source = Observable.from(numbers);
 
-class MyObserver {
-    next(value) {
-        console.log(`Value : ${value}`); 
-    }
-
-    error(e) {
-        console.log(`error: ${e}`); 
-    }
-
-    complete() {
-        console.log(`Complete`); 
-    }
-
-}
-
-source.subscribe(new MyObserver());
-source.subscribe(new MyObserver());
+source.subscribe(
+    value => console.log(`Value : ${value}`),
+    e => console.log(`error: ${e}`),
+    () => console.log(`complete`)
+);
