a31c7cbd27df861a62a8025ba9ea26f9586d2a89
Patricia Castillo <montero.hector@gmail.com>
2017-01-28 08:33:53 +0100

    add rxjs-extensions file that imports most common extensions.

---
+++ rxjs-extensions.ts
@@ -0,0 +1,8 @@
+import 'rxjs/add/operator/map'; 
+import 'rxjs/add/operator/filter'; 
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/retryWhen';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/scan';
+import 'rxjs/add/operator/takeWhile';
+import 'rxjs/add/observable/fromEvent';
