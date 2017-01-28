ef6741c54acc152072b36242916f747a292858a0
Patricia Castillo <montero.hector@gmail.com>
2017-01-28 08:28:38 +0100

    adds error handling and retry logic

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
+import './rxjs-extensions';
 
 let output = document.getElementById('output');
 let button = document.getElementById('button');
@@ -9,16 +10,32 @@ function load(url) {
     return Observable.create(observer => {
         let xhr = new XMLHttpRequest();
         xhr.addEventListener('load', () => {
-            let data = JSON.parse(xhr.responseText);
-            observer.next(data);
-            observer.complete();
+            if (xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText);
+                observer.next(data);
+                observer.complete();
+            } else {
+                observer.error(xhr.statusText);
+            }
         });
         xhr.open("GET", url);
         xhr.send();
-    });
+    }).retryWhen(retryStrategy({attemps: 3, delay: 1500 }));
 }
 
-function renderMovies(movies){
+function retryStrategy({ attemps = 3, delay = 1000 }){
+    return function(errors){
+        return errors
+                .scan((acc, value) => {
+                    console.log(acc, value); 
+                    return acc + 1; 
+                }, 0)
+                .takeWhile(acc => acc < attemps )
+                .delay(delay);
+    }
+}
+
+function renderMovies(movies) {
     movies.forEach(m => {
         let div = document.createElement('div');
         div.innerText = m.title;
@@ -26,11 +43,11 @@ function renderMovies(movies){
     });
 }
 
-function onError(e){
+function onError(e) {
     console.error(e);
 }
 
-function onComplete(){
+function onComplete() {
     console.log('complete');
 }
 
@@ -38,6 +55,5 @@ function onNext(value) {
     console.log(value);
 }
 
-
 clickStream.flatMap(e => load('movies.json'))
-           .subscribe(renderMovies, onError, onComplete)
+    .subscribe(renderMovies, onError, onComplete)
